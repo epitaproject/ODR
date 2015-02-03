17a30e6d34ff5c365f3265ae8bbef88d1f42eb21
Vincent Hudry <hudry_v@epita.fr>
2015-02-02 21:55:49 -0500

    rafraichir télécharge l'image dont l'url est dans la textbox, fenetre sdl l'affiche

--- ODR/interface/interface.c
+++ ODR/interface/interface.c
@@ -1,4 +1,4 @@
-#include <gtk/gtk.h>
+    #include <gtk/gtk.h>
 #include<SDL/SDL.h>
 #include<SDL/SDL_image.h>
 
@@ -11,7 +11,7 @@ void dsplsdl(GtkWidget *sender, gpointer data)
   
   if(i)
   {
-    displaysdl(gtk_entry_get_text(text));
+    displaysdl("dump.jpeg");
   }
   
   else
@@ -19,6 +19,20 @@ void dsplsdl(GtkWidget *sender, gpointer data)
   SDL_Quit();
   }
   
+}
+
+struct DLdata
+{
+  GtkWidget* text;
+  GtkWidget* interval;
+};
+
+void DL(GtkWidget *sender,gpointer data)
+{
+  struct DLdata *dt=data;
+  char* text=gtk_entry_get_text((GtkEntry*)(dt->text));
+  
+  download(text,"dump.jpeg");
 }
 
 void openmaingui()
@@ -39,10 +53,21 @@ void openmaingui()
   button=gtk_toggle_button_new();
   gtk_grid_attach(GTK_GRID (grid),button,0,1,1,1);
   gtk_button_set_label((GtkButton*)button,"fenêtre SDL");
-  g_signal_connect((GtkToggleButton*)button,"toggled",G_CALLBACK(dsplsdl),text);
+  g_signal_connect((GtkToggleButton*)button,"toggled",G_CALLBACK(dsplsdl),NULL);
   
+  GtkWidget* sleeper;
+  sleeper=gtk_spin_button_new_with_range(0,100,1);
+  gtk_grid_attach(GTK_GRID(grid),sleeper,1,0,1,1);
   
-  
+  struct DLdata *data=malloc(sizeof(struct DLdata));
+  data->text=text;
+  data->interval=sleeper;
+  GtkWidget* button2;
+  button2=gtk_toggle_button_new();
+  gtk_grid_attach(GTK_GRID (grid),button2,1,1,1,1);
+  gtk_button_set_label((GtkButton*)button2,"rafrachir");
+  g_signal_connect((GtkToggleButton*)button,"toggled",G_CALLBACK(DL),data);
+ 
   gtk_widget_show_all (window);
 }
 
