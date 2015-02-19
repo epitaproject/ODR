865dcfe09cba26e83213c6e7de0c83d361faa604
Merci <nicolas.mateos-mateos@epita.fr>
2015-02-19 16:14:23 +0100

    get/set pixel work+ flux video

--- ODR/camera/main.c
+++ ODR/camera/main.c
@@ -16,88 +16,55 @@ SDL_Surface* initsdl()
   return screen;
 }
 
-Uint32 getpixel(SDL_Surface *surface, int x, int y)
-{
-    int bpp = surface->format->BytesPerPixel;
-    /* Here p is the address to the pixel we want to retrieve */
-    Uint32 *p = (Uint32 *)surface->pixels + y * surface->pitch + x * bpp;
+static inline
+Uint8* pixelref(SDL_Surface *surf, unsigned x, unsigned y) {
+  int bpp = surf->format->BytesPerPixel;
+  return (Uint8*)surf->pixels + y * surf->pitch + x * bpp;
+}
 
-    switch(bpp) {
-    case 1:
-        return *p;
-        break;
+Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y) {
+  Uint8 *p = pixelref(surface, x, y);
+  switch(surface->format->BytesPerPixel) {
+  case 1:
+    return *p;
+  case 2:
+    return *(Uint16 *)p;
+  case 3:
+    if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+      return p[0] << 16 | p[1] << 8 | p[2];
+    else
+      return p[0] | p[1] << 8 | p[2] << 16;
+  case 4:
+    return *(Uint32 *)p;
+  }
+  return 0;
+}
 
-    case 2:
-        return *(Uint16 *)p;
-        break;
-
-    case 3:
-        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
-            return *p;
-        else
-            return *p;
-        break;
-
-    case 4:
-        return *(Uint32 *)p;
-        break;
-
-    default:
-        return 0;       /* shouldn't happen, but avoids warnings */
+void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel) {
+  Uint8 *p = pixelref(surface, x, y);
+  switch(surface->format->BytesPerPixel) {
+  case 1:
+    *p = pixel;
+    break;
+  case 2:
+    *(Uint16 *)p = pixel;
+    break;
+  case 3:
+    if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+      p[0] = (pixel >> 16) & 0xff;
+      p[1] = (pixel >> 8) & 0xff;
+      p[2] = pixel & 0xff;
+    } else {
+      p[0] = pixel & 0xff;
+      p[1] = (pixel >> 8) & 0xff;
+      p[2] = (pixel >> 16) & 0xff;
     }
+    break;
+  case 4:
+    *(Uint32 *)p = pixel;
+    break;
+  }
 }
-
-
-
-void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
-{
-    int bpp = surface->format->BytesPerPixel;
-    /* Here p is the address to the pixel we want to set */
-    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
-
-    switch(bpp) {
-    case 1:
-        *p = pixel;
-        break;
-
-    case 2:
-        *(Uint16 *)p = pixel;
-        break;
-
-    case 3:
-        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-            p[0] = (pixel >> 16) & 0xff;
-            p[1] = (pixel >> 8) & 0xff;
-            p[2] = pixel & 0xff;
-        } else {
-            p[0] = pixel & 0xff;
-            p[1] = (pixel >> 8) & 0xff;
-            p[2] = (pixel >> 16) & 0xff;
-        }
-        break;
-
-    case 4:
-        *(Uint32 *)p = pixel;
-        break;
-    }
-}
-Uint32* get_pixel32( SDL_Surface *surface, int x, int y )
-{
-    //Convert the pixels to 32 bit
-    Uint32 *pixels = (Uint32 *)surface->pixels;
-   // printf("%d\n",sizeof(pixels));
-    //Get the requested pixel
-    return pixels[ (y *surface->w) +x];
-}
-void put_pixel32( SDL_Surface *surface, int x, int y, Uint32 pixel )
-{
-    //Convert the pixels to 32 bit
-    Uint32 *pixels = (Uint32 *)surface->pixels ;
-    //Set the pixel
-    pixels[ ( y * surface->w ) + x ] = pixel;
-}
-
-
 
   void displaysdl(char*path, SDL_Surface *screen)
 {
@@ -117,12 +84,11 @@ void put_pixel32( SDL_Surface *surface, int x, int y, Uint32 pixel )
     {
         for(int j = 0; j < picture->h;j++)
         {
-         //   SDL_GetRGB(get_pixel32(picture,i,j),picture->format,&r,&g,&b);
-        //printf("%d\n",i);
-        //printf("%d\n",j);
-            myColor = get_pixel32(picture,i,j);
-         //  myColor = SDL_MapRGB(picture->format,200,200,200);
-            put_pixel32(dst,i,j,myColor);//((Uint32*)(picture->pixels))[j*(picture->h)+i]  );
+            if(i!=150)
+            {
+                myColor = getpixel(picture,i,j);
+            }
+            putpixel(dst,i,j,myColor);//((Uint32*)(picture->pixels))[j*(picture->h)+i]  );
         }
     }
 
@@ -163,7 +129,7 @@ int i = 0;
 while(i<1000)
 {
     printf("%d\n",i);
-    //download("http://172.21.1.200/cgi-bin/jpg/image.cgi?resolution=704x576&dummy=1422852582922","toto.jpg");
+    download("http://172.21.1.200/cgi-bin/jpg/image.cgi?resolution=704x576&dummy=1422852582922","toto.jpg");
 
     displaysdl("toto.jpg",screen);
     i++;
